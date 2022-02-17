0485986b63268f52d1132062464dd83958272ff1
Wenceslau Neto <wbeneto@ymail.com>
2022-02-17 10:06:15 -0300

    --Added project and readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # teacher-hello-world-spring-rest
 Projeto exemplo de uma app spring rest. para ensino.
+
+# Requisitos
+
+IntelliJ Community ou superior
+
+Maven local instalado na maquina
+
+Tutorial: https://dicasdejava.com.br/como-instalar-o-maven-no-windows/
+
+Java JDK 11
+
+
+# Executar
+
+executar o comando mvn spring-boot:run na pasta do projeto
+
+A console devera mostrar as linhas abaixo, e isso indica que aplicação iniciou
+
+INFO 9068 --- Tomcat started on port(s): 8080 (http) with context path '/spring-mvc'
+
+INFO 9068 --- Started Application in 1.421 seconds (JVM running for 1.711)
+
+
+# Testar
+
+Acessar os endereço no browser
+
+http://localhost:8080/spring-mvc/hello?nome=neto
---
+++ src/main/java/br/com/wenceslau/HelloRest.java
@@ -0,0 +1,38 @@
+package br.com.wenceslau;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRest {
+
+    @GetMapping
+    public Response hello(@RequestParam String nome) {
+
+        String msg = String.format("Hi %s. Spring Rest Get Request received: %s", nome, LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+
+        Response response = new Response();
+        response.setMensagem(msg);
+
+        return response;
+    }
+}
+
+class Response {
+    private String mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
